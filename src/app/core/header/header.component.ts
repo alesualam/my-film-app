c9033fb4663d0432c4a0e87b6aeca2d1528ac381
Suárez Lamadrid <asuarezl@indra.es>
2018-12-19 11:22:35 +0100

    form fixed

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import { DataStorageService } from 'src/app/shared/data-storage.service';
 import { HttpEvent } from '@angular/common/http';
+import { FilmService } from 'src/app/shared/films/films.service';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,7 @@ export class HeaderComponent implements OnInit {
   dropStatus = false;
   expandStatus = false;
 
-  constructor(private authService: AuthService, private storage: DataStorageService) { }
+  constructor(private authService: AuthService, private storage: DataStorageService, private filmService: FilmService) { }
 
   ngOnInit() {
   }
@@ -32,6 +33,8 @@ export class HeaderComponent implements OnInit {
 
   onLogout() {
     this.authService.logout();
+    this.filmService.editMode = false;
+    this.filmService.createMode = false;
   }
 
   onFetch() {
